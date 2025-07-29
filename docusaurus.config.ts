4cf0175f864ffb00fb35ca93ae4fbe1a4976bc49
Khatib Abbas <abbas_khatib@hotmail.com>
2025-07-29 23:16:18 +0200

    Updates edit URL to point to docs
    Updates the edit URL for both the main content and blog posts
    to point directly to the 'docs' directory in the repository. This
    streamlines the editing process, making it easier for contributors to
    find and modify documentation files.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,22 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/InnovateIt-ak/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/InnovateIt-ak/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+            'https://github.com/InnovateIt-ak/docusaurus/edit/master/docs',
         },
         theme: {
           customCss: './src/css/custom.css',
